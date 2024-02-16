cd156930cefc92bec785359c2ac84adb37b37b92
Phanupong Permpimol <phanupong@irobust.co.th>
2024-02-16 16:04:38 +0700

    update docker.tf

--- docker.tf
+++ docker.tf
@@ -13,11 +13,11 @@ provider "aws" {
   secret_key = "p8dALB3yC/5Vb8u/E+ifwzE6BXkzdDNArslT3PNK"
 }
 
-resource "aws_instance" "main" {
-  ami                                  = "ami-02eec49345a878486"
-  availability_zone                    = "ap-southeast-2b"
-  instance_type                        = "t2.micro"
+resource "aws_instance" "web" {
+  ami           = "ami-02eec49345a878486"
+  instance_type = "t2.micro"
+
   tags = {
-    Name = "terraform-cloud-webserver"
+    Name = "Terraform-cloud-webserver"
   }
 }
